fdd326d0e1437db80155d3fee827ec63c93b3c8d
mrcabo <dcgdiego@gmail.com>
2019-09-22 14:03:48 +0200

    Initializing with 400 agents (looks cooler)

--- crowd_evacuation/server.py
+++ crowd_evacuation/server.py
@@ -58,7 +58,7 @@ def agent_portrayal(agent):
 grid = CanvasGrid(agent_portrayal, 50, 50, 500, 500)
 
 model_params = {
-    "N": UserSettableParameter('slider', "Number of agents", 100, 2, 2000, 1,
+    "N": UserSettableParameter('slider', "Number of agents", 400, 2, 2000, 1,
                                description="Choose how many agents to include in the model"),
     "width": 50,
     "height": 50
